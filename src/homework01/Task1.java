cc91f240bb509d9cb8d4d5a87e2e67c1d27a79a3
Vladislav571 <agaeff.vlad@mail.ru>
2025-07-10 21:33:33 +0300

    changed 4 to 5

--- src/homework01/Task1.java
+++ src/homework01/Task1.java
@@ -2,7 +2,7 @@ package homework01;
 
     public class Task1 {
         public static void main(String[] args){
-            System.out.println("1   2   3   4");
+            System.out.println("1   2   3   5");
         }
     }
 
